798b149778c9cf7318be38ed727623866ebbea44
Gabriel Moreira Minossi <gordominossi@gmail.com>
2020-02-06 16:21:09 -0300

    refactor: adding fixed point function and removing the other newton functions

--- EP1.py
+++ EP1.py
@@ -3,81 +3,62 @@ import numpy as np
 MAX_ITER = 10
 EPSILON = 1e-8
 
-# Calcula uma raiz de um polinomio p usando o método de Newton
-# usando um número complexo x0 como aproximação inicial.
-# Recebe um polinômio p(x) do qual se quer saber um raiz
-# e uma aproximação inicial x0 da raiz.
-# Retorna uma raiz do polinômio p(x)
-# g = x - f(x)/df(x)
-def NewtonComAproximaçãoInicial(p, x0):
+def pontoFixo(p, g, x0):
     x = x0
-    # Calcula a derivada dp do polinômio p
-    dp = np.polyder(p)
+
     n_iter = 0
     while abs(np.polyval(p, x)) > EPSILON and n_iter < MAX_ITER:
-        # xk+1 = xk - f(xk)/df(xk)
-        x = x - np.polyval(p, x) / np.polyval(dp, x)
+        # xk+1 = g(xk)
+        x = g(x)
         n_iter += 1
-    return x
-
-# Calcula alguma raiz zl1 com o método de Newton
-# usando um número complexo aleatório como aproximação inicial.
-# Recebe um polinômio p(x) do qual se quer saber a raiz 
-# e um polinômio q(x) de fatores já encontrados do polinômio p(x).
-# Retorna uma raiz do polinômio deflacionado f(x) = p(x)/q(x)
-def NewtonComDeflação(p, q):
-    dp = np.polyder(p)
-    dq = np.polyder(q)
-    # Encontrar uma raiz != NaN
-    zl1 = np.nan
-    while np.isnan(zl1):
-        # x0 é um complexo aleatório de módulo entre 0 e 1
-        x = complex(np.random.rand(), np.random.rand())
-        
-        # Para se |p(x)| > EPSILON ou se n_iter >= MAX_ITER
-        n_iter = 0
-        while abs(np.polyval(p, x)) >= EPSILON and n_iter < MAX_ITER:
-            # xk1 = xk - 1/(dp/p - dq/q)
-            x = x - 1/(np.polyval(dp, x)/np.polyval(p, x) - np.polyval(dq, x)/np.polyval(q, x))
-            n_iter += 1
-
-        # Se n_iter > MAX_ITER, ainda não achou uma raiz de f = p/q
-        if n_iter > MAX_ITER:
-            zl1 = np.nan
-        else:
-            zl1 = x
-    return zl1
+    if n_iter >= MAX_ITER:
+        return np.nan
+    else: 
+        return x
 
 def polyzeros(a):
-    p = np.poly1d(a)
-    print(p)
-    zeros = []
+    p = np.poly1d(a, 1)
+    dp = np.polyder(p)
 
+    # g(x) = x - p(x) / dp(x)
+    g = lambda x : x - np.polyval(p, x) / np.polyval(dp, x)
+    
     # Calcula a aproximação da primeira raiz com o método de Newton
     # usando um número complexo aleatório como aproximação inicial
-    raiz = NewtonComAproximaçãoInicial(p, complex(np.random.rand(), np.random.rand()))
-    zeros.append(raiz)
+    raiz = pontoFixo(p, g, complex(np.random.rand(), np.random.rand()))
+    
+    raizes = [raiz]
 
     # q = polinômio de fatores do polinômio p encontrados até agora
-    q = np.poly1d(zeros, True)
+    q = np.poly1d(raizes, True)
+    dq = np.polyder(q)
 
     # Encontra as outras raízes
-    # Adiciona raizes ao vetor de zeros enquanto ele for menor que
+    # Adiciona raizes ao vetor de raizes enquanto ele for menor que
     # a lista de coeficientes de a (que tem a - 1 raizes)
-    while(len(zeros) < len(a) - 1):
-        # Calcula alguma raiz zl1 != NaN com o método de Newton
-        # do polinômio deflacionado f(x) = p(x)/q(x)
-        zl1 = NewtonComDeflação(p, q)
+    while(len(raizes) < len(a)):
+        
+        # Encontrar uma raiz != NaN usando o método de Newton para o polinômio p(x)/q(x)
+        zl1 = np.nan
+        while np.isnan(zl1):
+            # x0 é um complexo aleatório de módulo entre 0 e 1
+            x0 = complex(np.random.rand(), np.random.rand())
+            
+            # g(x) = p(x) / q(x)
+            g = lambda x : x - 1/(np.polyval(dp, x)/np.polyval(p, x) - np.polyval(dq, x)/np.polyval(q, x))
+            zl1 = pontoFixo(p, g, x0)
 
+        g = lambda x : x - np.polyval(p, x) / np.polyval(dp, x)
         # Calcula uma raiz com o método de Newton
         # usando zl1 como aproximação inicial
-        zero = NewtonComAproximaçãoInicial(p, zl1)
-        zeros.append(zero)
+        raiz = pontoFixo(p, g, zl1)
+        raizes.append(raiz)
 
         # Recalcula o polinômio q(x) com a nova raiz encontrada
-        q = np.poly1d(zeros, True)
+        q = np.poly1d(raizes, True)
+        dq = np.polyder(q)
 
-    return zeros
+    return raizes
 
 def main():
     f = open("entrada.txt", "r")
