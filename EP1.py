387b56c9446f1fe8e0db377a3c46c41eb3cafa7c
Gabriel Moreira Minossi <gordominossi@gmail.com>
2020-02-13 17:14:23 -0300

    refactor: moving the definition of g's to separate functions

--- EP1.py
+++ EP1.py
@@ -17,50 +17,58 @@ def pontoFixo(p, g, x0):
         n_iter += 1
     return x
 
+# Método de Newton para um polinômio p(x)
+# Recebe p(x) e x0 e retorna uma raiz de p(x)
+def newtonClassico(p, x0):
+    dp = np.polyder(p)
+
+    # g(x) = x - p(x) / p'(x)
+    g = lambda x : x - np.polyval(p, x) / np.polyval(dp, x)
+
+    return pontoFixo(p, g, x0)
+
+
+# Calcula usa o Método de Newton para a função f(x) = p(x) / q(x), em que 
+# q(x) é o polinốmio de fatores já encontrados de p(x) dado pelo vetor de raizes
+# Recebe o polinômio p(x), um vetor das raizes já encontradas e
+# Retorna uma aproximação de uma raiz nova de p(x)
+def newtonPolinomial(p, raizes):
+    dp = np.polyder(p)
+
+    # x0 é um complexo aleatório de módulo entre 0 e 1
+    x0 = complex(np.random.rand(), np.random.rand())
+
+    # dqq(x) = q'(x) / q(x) = soma das frações 1 / (x - raiz) para cada raiz já encontrada
+    dqq = lambda x : sum(list(map(lambda raiz : 1 / np.polyval(np.poly1d([raiz], True), x), raizes)))
+
+    # f(x) = p(x) / q(x)
+    # g(x) = x - f(x) / f'(x) = 1 / (p'(x) / p(x) - q'(x) / q(x))
+    g = lambda x : x - 1 / (np.polyval(dp, x) / np.polyval(p, x) - dqq(x))
+
+    return pontoFixo(p, g, x0)
+
+
 
 # Retorna o vetor de raizes do polinômio p(x) de coeficientes dados pelo vetor a
 def polyzeros(a):
     p = np.poly1d(a)
-    dp = np.polyder(p)
-
-    # g(x) = x - p(x) / dp(x)
-    g = lambda x : x - np.polyval(p, x) / np.polyval(dp, x)
 
     # Calcula a aproximação da primeira raiz com o método de Newton
     # usando um número complexo aleatório como aproximação inicial
-    raiz = pontoFixo(p, g, complex(np.random.rand(), np.random.rand()))
+    raiz = newtonClassico(p, complex(np.random.rand(), np.random.rand()))
     raizes = [raiz]
 
-    # q(x) é o polinômio de fatores do polinômio p encontrados até agora
-    # dqq = q'(x) / q(x)
-    dqq = lambda x : 1 / np.polyval(np.poly1d([raiz], True), x)
 
     # Encontra as outras raízes
     # Adiciona raizes ao vetor de raizes [z1, ..., zn] enquanto ele for menor que
     # a lista de coeficientes de a [a0, ..., an]
     while(len(raizes) < len(a) - 1):
-        # x0 é um complexo aleatório de módulo entre 0 e 1
-        x0 = complex(np.random.rand(), np.random.rand())
-
-        # f(x) = p(x) / q(x)
-        # g(x) = f(x) / f'(x)
-        g = lambda x : x - 1 / (np.polyval(dp, x) / np.polyval(p, x) - dqq(x))
         # Encontra uma raiz usando o método de Newton para o polinômio p(x)/q(x)
-        zl1 = pontoFixo(p, g, x0)
+        zl1 = newtonPolinomial(p, raizes)
 
-        # f(x) = p(x)
-        # g(x) = f(x) / f'(x)
-        g = lambda x : x - np.polyval(p, x) / np.polyval(dp, x)
-        # Calcula uma raiz com o método de Newton
-        # usando zl1 como aproximação inicial
-        raiz = pontoFixo(p, g, zl1)
+        # Refina a aproximação zl1 da raiz
+        raiz = newtonClassico(p, zl1)
         raizes.append(raiz)
-
-        # Recalcula dqq(x) com a nova raiz encontrada,
-        #############################################
-        ### FAVELA VAI COLOCAR A PROPRIEDADE AQUI ###
-        #############################################
-        dqq = lambda x : sum(list(map(lambda raiz : 1 / np.polyval(np.poly1d([raiz], True), x), raizes)))
 
     return raizes
 
@@ -92,9 +100,11 @@ def main():
     raizes = np.roots(coeficientes)
     plot(raizes, "rs", "numpy.roots", coeficientes)
 
+    print("raizes numpy.roots: ", raizes)
+
     raizes = polyzeros(coeficientes)
     plot(raizes, "g*", "polyzeros", coeficientes)
 
-    print("raizes: ", raizes)
+    print("raizes polyzeros: ", raizes)
 
 main()
